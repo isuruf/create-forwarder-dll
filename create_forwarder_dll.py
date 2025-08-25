110c71e6a8effa16156bff882afe5308f3663c09
Isuru Fernando <isuruf@gmail.com>
2025-08-25 09:00:47 -0400

    use target_platform env variable

--- create_forwarder_dll.py
+++ create_forwarder_dll.py
@@ -4,9 +4,29 @@ import subprocess
 import sys
 
 PROCESSOR_ARCHITECTURE = os.environ.get("PROCESSOR_ARCHITECTURE", "")
+target_platform = os.environ.get("target_platform", None)
+
+target_platform_map = {
+  "win-64": "x64",
+  "win-arm64": "ARM64",
+  "win-32": "X86",
+}
+
+processor_architecture_map = {
+  "amd64": "x64",
+  "arm64": "ARM64",
+  "x86": "X86",
+}
 
 def run(arg):
   return subprocess.check_output(arg, shell=True).decode("utf-8")
+
+
+def get_machine_default():
+  if target_platform:
+    return target_platform_map[target_platform]
+  else:
+    return processor_architecture_map.get(PROCESSOR_ARCHITECTURE.lower(), "")
 
 
 def parse_args(args):
@@ -16,7 +36,7 @@ def parse_args(args):
   )
   parser.add_argument('input', help="path to input DLL")
   parser.add_argument('output', help="path to output DLL")
-  parser.add_argument('--arch', default=PROCESSOR_ARCHITECTURE)
+  parser.add_argument('--machine', default=get_machine_default())
   parser.add_argument('--no-temp-dir', action='store_true')
   return parser.parse_args(args)
 
@@ -31,15 +51,6 @@ def main(args):
   output_dir = os.path.dirname(args.output)
   assert output_dll.endswith(".dll")
   output = os.path.basename(output_dll)[:-4]
-  
-  if args.arch.lower() == "amd64":
-    machine = "x64"
-  elif args.arch.lower() == "arm64":
-    machine = "ARM64"
-  elif args.arch.lower() == "x86":
-    machine = "X86"
-  else:
-    raise NotImplementedError(f"Unknown arch")
   
   # create empty object file to which we can attach symbol export list
   open("empty.c", "a").close()
@@ -66,7 +77,7 @@ def main(args):
       f.write(f"  {symbol}\n")
       
   # create import library with that list of symbols
-  run(f"lib /def:{input}.def /out:{input}.lib /MACHINE:{machine}")
+  run(f"lib /def:{input}.def /out:{input}.lib /MACHINE:{args.machine}")
   
   # create DLL from empty object and the import library
   with open(f"{output}.def", "w") as f:
@@ -77,7 +88,7 @@ def main(args):
 
   cl_exe = run("where cl.exe")
   link_exe = os.path.join(os.path.dirname(cl_exe), "link.exe")
-  run(f"\"{link_exe}\" /DLL /OUT:{output}.dll /DEF:{output}.def /MACHINE:{machine} empty.obj {input}.lib")
+  run(f"\"{link_exe}\" /DLL /OUT:{output}.dll /DEF:{output}.def /MACHINE:{args.machine} empty.obj {input}.lib")
   run(f"copy {output}.dll {output_dll}")
 
 if __name__ == "__main__":
